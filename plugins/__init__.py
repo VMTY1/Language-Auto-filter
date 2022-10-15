7d5cefe22bfa48a1b394ecf0ca9c84b110bba465
Manjunath Bhat <86521081+Bhatmanjusms@users.noreply.github.com>
2022-10-15 21:54:36 +0530

    Add files via upload

---
+++ plugins/__init__.py
@@ -0,0 +1,21 @@
+from plugins.malik2.buttons import HSTN,  HSTNN, NOTFOUN
+from info import BOT_USERNAME, DEL_SECOND
+from plugins.malik.extra import IYGL, PPI, SMART_PIC, REQUEST_ADMIN 
+from Script import MNTFN 
+
+FORCESUB_TXT = """malik"""
+
+
+class malik(object):
+      hstn = HSTN
+      hstnn = HSTNN
+      u_name = BOT_USERNAME 
+      int_link = IYGL
+      smart_pic = SMART_PIC 
+      delete = DEL_SECOND
+      notfoun = REQUEST_ADMIN 
+      mntf = MNTFN
+      ntatmo = FORCESUB_TXT
+      ntatm =  PPI
+
+
