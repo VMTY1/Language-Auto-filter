17d4e56dee67bc7cd123c4b2098eac53763a1334
Akshay962 <107416405+Akshay962@users.noreply.github.com>
2022-11-30 14:45:09 +0530

    Update buttons.py

--- plugins/malik2/buttons.py
+++ plugins/malik2/buttons.py
@@ -5,13 +5,13 @@ from os import environ
 
 
 
-NOTFOUN = InlineKeyboardMarkup([[InlineKeyboardButton("Request", url="https://t.me/m_admins")]]) 
+NOTFOUN = InlineKeyboardMarkup([[InlineKeyboardButton("Request", url="https://t.me/iPapPrimeSPbot")]]) 
 
-GSLB = InlineKeyboardMarkup([[InlineKeyboardButton("♻️ 𝐉𝐨𝐢𝐧 𝙂𝙧𝙤𝙪𝙥 ", url="https://t.me/+gXuMKXOWm1UyOTdl")],[ InlineKeyboardButton("♻️ 𝐉𝐨𝐢𝐧 𝙂𝙧𝙤𝙪𝙥 ", url="https://t.me/+gXuMKXOWm1UyOTdl")]]) 
+GSLB = InlineKeyboardMarkup([[InlineKeyboardButton("♻️ 𝐉𝐨𝐢𝐧 𝙂𝙧𝙤𝙪𝙥 ", url="https://t.me/+7p7DwzUq5WdmYWU1")],[ InlineKeyboardButton("♻️ 𝐉𝐨𝐢𝐧 𝙂𝙧𝙤𝙪𝙥 ", url="https://t.me/+7p7DwzUq5WdmYWU1")]]) 
 
-HSTN = InlineKeyboardMarkup([[InlineKeyboardButton("❇️ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs ❇️", url=f"http://t.me/{malikk.u_name}?startgroup=true") ],[ InlineKeyboardButton('♻️ ʜᴇʟᴘ ♻️', callback_data='help'), InlineKeyboardButton('⚡️ᴜᴘᴅᴀᴛᴇs⚡️', url='https://t.me/m_house786')],[InlineKeyboardButton('sᴇᴀʀᴄʜ ɪɴʟɪɴᴇ', switch_inline_query_current_chat=''), InlineKeyboardButton('♻️ ᴀʙᴏᴜᴛ ♻️', callback_data='about')],[ InlineKeyboardButton('✅ sᴜʙsᴄʀɪʙᴇ ᴍʏ ʏᴛ ᴄʜᴀɴɴᴇʟ ✅', url='https://youtube.com/channel/UCPaHDqWf3D3w2nxb8p3sr4A')]])  
+HSTN = InlineKeyboardMarkup([[InlineKeyboardButton("❇️ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs ❇️", url=f"http://t.me/{malikk.u_name}?startgroup=true") ],[ InlineKeyboardButton('♻️ ʜᴇʟᴘ ♻️', callback_data='help'), InlineKeyboardButton('⚡️ᴜᴘᴅᴀᴛᴇs⚡️', url='https://t.me/iPapkornOfficial')],[InlineKeyboardButton('♻️ ᴀʙᴏᴜᴛ ♻️', callback_data='about')]])  
 
-HSTNN = InlineKeyboardMarkup([[InlineKeyboardButton('❇️ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs ❇️', url=f'http://t.me/{malikk.u_name}?startgroup=true') ],[ InlineKeyboardButton('♻️ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ ♻️', url='https://t.me/m_house786')],[ InlineKeyboardButton('🔹 ʜᴇʟᴘ 🔹', url=f"http://t.me/{malikk.u_name}?start=help")]])
+HSTNN = InlineKeyboardMarkup([[InlineKeyboardButton('❇️ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs ❇️', url=f'http://t.me/{malikk.u_name}?startgroup=true') ],[ InlineKeyboardButton('♻️ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ ♻️', url='https://t.me/iPapkornOfficial')],[ InlineKeyboardButton('🔹 ʜᴇʟᴘ 🔹', url=f"http://t.me/{malikk.u_name}?start=help")]])
 
 
 
