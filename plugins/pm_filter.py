d75e02ccaf9dc0b36450c6b46fc96c872808c4fa
Akshay962 <107416405+Akshay962@users.noreply.github.com>
2022-12-05 05:40:55 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -34,12 +34,10 @@ SPELL_CHECK = {}
 
 
 
-@Client.on_message(filters.group & filters.text & filters.incoming)
+@Client.on_message((filters.group | filters.private) & filters.text & filters.incoming)
 async def give_filter(client, message):
-    settings = await get_settings(message.chat.id)
-    if settings['manual_filter']:
-        await manual_filters(client, message)
-    if settings['auto_filters']:
+    k = await manual_filters(client, message)
+    if k == False:
         await auto_filter(client, message)
 
 
